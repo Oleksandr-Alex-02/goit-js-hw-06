3af36f4c47deb358ecaf177ade4eee4e4f3b881a
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-07-08 18:27:21 +0300

    task-1+2

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,12 @@
-const categories = document.querySelectorAll(".item, h2");
-console.log(categories);
+const amount = document.querySelectorAll("#categories .item");
+const number = `Number of categories: ${amount.length}`;
+console.log(number);
+
+const tatel = document.querySelectorAll(".item h2");
+tatel.forEach(function (number) {
+  const array = `Category: ${number.textContent}`;
+  console.log(array);
+  const tate = document.querySelectorAll(".item .animals .item__li");
+  const arr = `Elements: ${tate.length}`;
+  console.log(arr);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+ingredients.forEach(function callback(element) {
+  const newLi = document.createElement("li");
+  newLi.classList.add("item");
+  newLi.textContent = element;
+  console.log(newLi);
+});
