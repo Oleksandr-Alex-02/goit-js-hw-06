de9b31a8fb2aa6e87aa80f4e520fbbb1880d10ad
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-07-10 12:58:41 +0300

    tack-1_+-

--- js/task-01.js
+++ js/task-01.js
@@ -2,11 +2,17 @@ const amount = document.querySelectorAll("#categories .item");
 const number = `Number of categories: ${amount.length}`;
 console.log(number);
 
+const array = document.querySelectorAll(".item ul");
+
+array.forEach(function callbackfn(items) {
+  const elementLi = items.children;
+  const number = `Elements: ${elementLi.length}`;
+  console.log(number);
+});
+
 const tatel = document.querySelectorAll(".item h2");
+
 tatel.forEach(function (number) {
   const array = `Category: ${number.textContent}`;
   console.log(array);
-  const tate = document.querySelectorAll(".item .animals .item__li");
-  const arr = `Elements: ${tate.length}`;
-  console.log(arr);
 });
