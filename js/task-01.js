fd8162100117f2628359cd9aa5948c6e0a89842d
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-07-10 21:59:36 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,25 @@
-const amount = document.querySelectorAll("#categories .item");
-const number = `Number of categories: ${amount.length}`;
-console.log(number);
+// const amount = document.querySelectorAll("#categories .item");
+// const number = `Number of categories: ${amount.length}`;
+// console.log(number);
 
-const array = document.querySelectorAll(".item ul");
+// const tatel = document.querySelectorAll(".item h2");
+// const array = document.querySelectorAll(".item ul");
 
-array.forEach(function callbackfn(items) {
-  const elementLi = items.children;
-  const number = `Elements: ${elementLi.length}`;
-  console.log(number);
-});
+// array.forEach(
+//   function callbackfn(items) {
+//     const elementLi = items.children;
+//     const number = `Elements: ${elementLi.length}`;
+//     console.log(number);
+//   },
+//   tatel.forEach(function callbackfn(number) {
+//     const array = `Category: ${number.textContent}`;
+//     console.log(array);
+//   })
+// );
 
-const tatel = document.querySelectorAll(".item h2");
-
-tatel.forEach(function (number) {
-  const array = `Category: ${number.textContent}`;
-  console.log(array);
-});
+const categories = document.querySelector("#categories");
+console.log(categories);
+const categ = categories.querySelector(".item");
+console.log(categ);
+const cat = categ.querySelectorAll("li").length;
+console.log(cat);
