9c2ad3f92d48ec3c6afb842fa89caddb6c71f89c
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-07-23 12:21:23 +0300

    task-5-+

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+const textInput = document.querySelector("#name-input");
+console.log(textInput);
+
+const span = document.querySelector("#name-output");
+console.log(span.textContent);
+
+textInput.addEventListener("input", (event) => {
+  span.textContent = event.currentTarget.value;
+});
+
+textInput.addEventListener("blur", (event) => {
+  if (textInput.value === "") {
+    span.textContent = "Anonymous";
+  }
+});
