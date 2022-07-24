e1c3133c446bd3670c7308c8a41f5bd82eeac840
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-07-24 17:48:02 +0300

    task-6-+

--- js/task-05.js
+++ js/task-05.js
@@ -1,15 +1,15 @@
-const textInput = document.querySelector("#name-input");
+const textInput = document.querySelector('#name-input');
 console.log(textInput);
 
-const span = document.querySelector("#name-output");
+const span = document.querySelector('#name-output');
 console.log(span.textContent);
 
-textInput.addEventListener("input", (event) => {
+textInput.addEventListener('input', event => {
   span.textContent = event.currentTarget.value;
 });
 
-textInput.addEventListener("blur", (event) => {
-  if (textInput.value === "") {
-    span.textContent = "Anonymous";
+textInput.addEventListener('blur', event => {
+  if (textInput.value === '') {
+    span.textContent = 'Anonymous';
   }
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,12 @@
-function sasa(a, b, c) {
-  return a + b + c;
-}
+const onInput = document.querySelector('#validation-input');
+const lengthInput = onInput.getAttribute('data-length');
 
-const array = [1, 2, 3];
-console.log(sasa(...array));
+onInput.addEventListener('input', event => {
+  onInput.addEventListener('blur', event => {
+    if (event.currentTarget.value.length >= Number(lengthInput)) {
+      onInput.setAttribute('id', 'validation-input.valid');
+    } else {
+      onInput.setAttribute('id', 'validation-input.invalid');
+    }
+  });
+});
